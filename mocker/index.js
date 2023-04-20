269663792231867edd3ce73d70e5069d8a6e3981
Your Name <1758555877@qq.com>
2023-04-20 23:04:07 +0800

    添加后端数据

--- mocker/index.js
+++ mocker/index.js
@@ -8,6 +8,7 @@ const card = require('./card');
 const classcard = require('./classcard');
 const username = require('./username');
 const classare = require('./classare');
+const data = require('./tuijian');
 
 
 const proxy = {
@@ -19,44 +20,6 @@ const proxy = {
     ...classcard,
     ...classare,
     ...username,
-
-
-    'GET /api/user/': [
-        {
-            "name":"ps",
-            "CourseName": "[ps大师班,告别小白]",
-            "imgurl": "https://s1.ax1x.com/2023/03/27/ppsMeeg.jpg",
-            "speaker": "千小喵",
-            "deleteAt": "2022/12/01",
-        },
-        {
-            "name":"p ",
-            "CourseName": "[手绘设计30天速成班]",
-            "imgurl": "https://s1.ax1x.com/2023/03/27/ppstBp4.jpg",
-            "speaker": "千小喵",
-            "Trash": "2022/12/01",
-        },
-        {
-            "CourseName": "[平面设计30天速成班]",
-            "imgurl": "https://s1.ax1x.com/2023/03/27/ppstakT.jpg",
-            "speaker": "千小喵",
-            "favorties": true,
-        },
-        {
-            "CourseName": "[PS初级入门到精通]",
-            "imgurl": "https://s1.ax1x.com/2023/03/27/ppstdtU.jpg",
-            "speaker": "千小喵",
-        },
-        {
-            "CourseName": "[1天练就Sketch高手]",
-            "imgurl": "https://s1.ax1x.com/2023/03/27/ppstwhF.jpg",
-            "speaker": "千小喵",
-        },
-        {
-            "CourseName": "[AE初级入门到精通]",
-            "imgurl": "https://s1.ax1x.com/2023/03/27/ppstBp4.jpg",
-            "speaker": "千小喵",
-        },
-    ],
+    ...data,
 }
 module.exports = (noProxy ? {} : delay(proxy, 10));
